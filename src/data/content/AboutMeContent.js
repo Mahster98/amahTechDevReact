b7b4baec180cdbfadced22f787bcc37ac7851a22
Mahster98 <mahalex88@gmail.com>
2020-04-19 15:09:41 -0600

    Grammatical and Formatting Changes

--- src/data/content/AboutMeContent.js
+++ src/data/content/AboutMeContent.js
@@ -2,7 +2,7 @@ import aboutMePic from '../img/profPic1.jpg'
 
 export default [
         {
-            "bio":"I am a fourth year Software Engineering Student at the Schulich School of Engineering. I am able to develop in full stack design using technologies such as Angular, React, Apache and Node.js. I am also well versed in object oriented languages like Java and C/C++ and can develop applications with these tools as well. I see programming as a fun and exciting tool to help me explore emerging technology and to improve our lives in an increasingly dynamic and interconnected world. Other than being in front of a computer, I also enjoy spending my free time cooking in the kitchen. I am constantly brainstorming for a project to combine my two passions into one!",
+            "bio":"I am a fourth year Software Engineering Student at the Schulich School of Engineering. I am able to develop in full stack design using technologies such as Angular, React, Apache and Node.js. I am also well versed in object oriented languages like Java and C/C++ and can develop applications with these tools as well. I see programming as a fun and exciting tool to help me explore emerging technology and to improve our lives in an increasingly dynamic and interconnected world. When I am not infront of a computer, I also enjoy spending my free time cooking in the kitchen. I am constantly brainstorming for a project to combine my two passions into one!",
             "image": aboutMePic,
             "linkedin": "https://www.linkedin.com/in/alexander-mah/",
             "github": "https://github.com/Mahster98",
