a0e236050b8a45b12d27b46ad3bb207cd9820136
Mahster98 <mahalex88@gmail.com>
2020-04-19 19:52:03 -0600

    Grammatical and Formatting Changes Part 2

--- src/data/content/ProjectContent.js
+++ src/data/content/ProjectContent.js
@@ -6,14 +6,14 @@ export default [
             "projectTitle": "Amahtech.dev",
             "date": "Deployed on February 28th, 2019",
             "tech": ["HTML", "CSS", "React", "Node.JS"],
-            "projectDescription": "I decided to start a project to create a website, just for fun! It has taught me quite a bit about web technologies! It’s a great opportunity for me to practice not only my HTML and CSS skills, but my front end design as well (this is built using React as a framework). I aim to continually update, redesign and improve not only the content but the user experience of this site as well so stay turned for changes!",
+            "projectDescription": "I decided to start a project to create a website, just for fun! As an added bonus, it has also taught me quite a bit about web technologies! It’s a great opportunity for me to practice my HTML and CSS skills, as well as my front-end design (this is built using React as a framework). I aim to continually update, redesign and improve not only the content but the user experience of this site as well so stay tuned for changes!",
             "image": amahTechPic
         },
         {
             "projectTitle": "Blockwork Circuits",
             "date": "Developed Summer 2017",
             "tech": ["Java", "Android Studio"],
-            "projectDescription": "This was a small game that I created under a NSERC (Natural Sciences and Engineering Research Council) grant. The aim of the game was to draw paths to lead the black square to the red squares, which helps illustrate problems that happen when trying to optimize the clock in modern circuits. I enjoyed this project because it was a great experience to develop designs and ideas independently and end up with a final project that I could show on my phone!",
+            "projectDescription": "This was a small game that I created under a NSERC (Natural Sciences and Engineering Research Council) grant. The aim of the game is to draw paths to lead the black square to the red squares, which helps illustrate problems that happen when trying to optimize the clock in modern circuits. I enjoyed this project because it was a great experience to develop designs and ideas independently and end up with a final project that I could show on my phone!",
             "image": blockworkCircuits
         },
 
